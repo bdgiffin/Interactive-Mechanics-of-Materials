996806bb395ad47e495c727a14c1ee6f738ee96e
Brian Giffin <bdgiffin@Brians-MBP.attlocal.net>
2025-09-09 11:45:50 -0500

    Updated Mohr's circle example.

--- Mohr_circle/main.py
+++ Mohr_circle/main.py
@@ -2,27 +2,39 @@ import pygame
 import asyncio
 from math import *
 
+# Include the pygame_widgets module
+import pygame_widgets
+from pygame_widgets.slider  import Slider
+
 pygame.init()
 window_width  = 1600
 window_height = 800
 window = pygame.display.set_mode((window_width,window_height))
 
+# Create a slider
+Nsteps = 50
+sxx_slider = Slider(window,210, 20, 440, 15,
+                    min=0, max=2*Nsteps, step=1, initial=int(Nsteps+0.7*Nsteps))
+syy_slider = Slider(window,210, 50, 440, 15,
+                    min=0, max=2*Nsteps, step=1, initial=int(Nsteps-0.4*Nsteps))
+sxy_slider = Slider(window,210, 80, 440, 15,
+                    min=0, max=2*Nsteps, step=1, initial=int(Nsteps+0.9*Nsteps))
+
 async def main():
 
     # Initialize a new font for drawing text
     pygame.font.init()
-    font_type  = 'PT Mono'
+    font_type  = 'None'
     font_size  = 30 # (pixels)
     stress_font = pygame.font.SysFont(font_type, font_size)
 
     # Create rendered text with the title of the game
-    game_title    = "Mohr's circle"
+    game_title    = "Mohr's circle σ-x τ-xy"
     anti_aliasing = True # (adjusts pixelized appearance of rendered text)
     text_color    = "Black"
     title_text = stress_font.render(game_title, anti_aliasing, text_color)
 
     # Define Mohr's circle dimensions
-
     circle_center = (int(window_width*0.2),int(window_height/2))
 
     # Define inner and outer block dimensions
@@ -41,20 +53,41 @@ async def main():
         theta  = atan2(dx[1],dx[0])
 
         # define points of the arrow
-        if (length > 1):
-            points = [[0,          -width/2],
-                      [length,     -width/2],
-                      [length,     -size/2 ],
-                      [length+size, 0      ],
-                      [length,     +size/2 ],
-                      [length,     +width/2],
-                      [0,          +width/2]]
-        else:
-            points = [[0,0],[0,0],[0,0]]
+        points = [[0,          -width/2],
+                  [length,     -width/2],
+                  [length,     -size/2 ],
+                  [length+size, 0      ],
+                  [length,     +size/2 ],
+                  [length,     +width/2],
+                  [0,          +width/2]]
 
         # return the rotated and translated arrow
         points = rotate(points,theta)
         return translate(points,x0)
+
+    def draw_arrow(x0, dx, width, size, color, label="", label_offset=0):
+        length = sqrt(dx[0]*dx[0]+dx[1]*dx[1])
+        theta  = atan2(dx[1],dx[0])
+
+        # only draw the arrow if it is long enough
+        if (length > 1):
+            pygame.draw.polygon(window, color, arrow(x0,dx,width,size))
+            label_text = stress_font.render(label, False, color)
+            text_rect = label_text.get_rect()
+            text_rect.center = (x0[0]+(length+label_offset)*cos(theta),x0[1]-(length+label_offset)*sin(theta))
+            window.blit(label_text,text_rect)
+
+    def draw_arc(x0,r,theta0,dtheta,negative,color,label="{:.0f}°",label_offset=0):
+        # only draw the angle if it is large enough and radius is non-zero
+        ddegrees = abs(dtheta*180/pi)
+        if ((ddegrees > 1) and (r > 0)):
+            pygame.draw.arc(window, color, (x0[0]-r, x0[1]-r, 2*r, 2*r), -theta0, -(theta0+dtheta))
+            if (negative):
+                ddegrees = -ddegrees
+            label_text = stress_font.render(label.format(ddegrees), False, color)
+            text_rect = label_text.get_rect()
+            text_rect.center = (x0[0]+(r+label_offset)*cos(-theta0-0.5*dtheta),x0[1]-(r+label_offset)*sin(-theta0-0.5*dtheta))
+            window.blit(label_text,text_rect)
 
     def translate(points,dx):
         for i, point in enumerate(points):
@@ -68,15 +101,16 @@ async def main():
         return points
 
     # Define the global stresses
-    stress_scale = 5
-    sxx =  10*stress_scale
-    syy = -5*stress_scale
-    sxy =  15*stress_scale
+    max_stress_value = 1
+    sxx = max_stress_value*(sxx_slider.getValue()-Nsteps)
+    syy = max_stress_value*(syy_slider.getValue()-Nsteps)
+    sxy = max_stress_value*(sxy_slider.getValue()-Nsteps)
 
     mouse_pressed = False
     game_running = True
     while game_running:
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 game_running = False
             if ((event.type == pygame.MOUSEBUTTONDOWN) or (event.type == pygame.FINGERDOWN)):
@@ -89,6 +123,11 @@ async def main():
             if (mouse_x > 0.5*window_width):
                 theta = atan2(mouse_y-block_center[1],mouse_x-block_center[0])
 
+        # Update stresses defined in primary x,y coordinate frame
+        sxx = max_stress_value*(sxx_slider.getValue()-Nsteps)
+        syy = max_stress_value*(syy_slider.getValue()-Nsteps)
+        sxy = max_stress_value*(sxy_slider.getValue()-Nsteps)
+
         # Compute stress invariants
         I1 = sxx + syy
         I2 = sxx*syy + sxy*sxy
@@ -98,6 +137,9 @@ async def main():
         tmax = sqrt(0.25*(sxx-syy)*(sxx-syy) + sxy*sxy)
         s1 = savg + tmax
         s2 = savg - tmax
+
+        # Compute the rotation angle to the principal stress configuration
+        thetap = 0.5*atan2(sxy,sxx-savg)
 
         # Update Euler angles
         cost = cos(-theta)
@@ -129,73 +171,98 @@ async def main():
                        (int(block_center[0]+inner_block_size*cos(theta-1*pi/4)),int(block_center[1]+inner_block_size*sin(theta-1*pi/4))),
                        (int(block_center[0]+inner_block_size*cos(theta+1*pi/4)),int(block_center[1]+inner_block_size*sin(theta+1*pi/4))),
                        (int(block_center[0]+inner_block_size*cos(theta+3*pi/4)),int(block_center[1]+inner_block_size*sin(theta+3*pi/4))))
+        x_axis = (int(0.25*inner_block_size),0)
+        y_axis = (0,int(0.25*inner_block_size))
         xprime_axis = (int(+0.25*inner_block_size*cos(theta+0*pi/2)),int(-0.25*inner_block_size*sin(theta+0*pi/2)))
         yprime_axis = (int(-0.25*inner_block_size*cos(theta+1*pi/2)),int(+0.25*inner_block_size*sin(theta+1*pi/2)))
 
         # Draw inner block
         pygame.draw.polygon(window, "Light Steel Blue", inner_block)
+
+        # Draw axes
         axis_width = 4
         axis_size  = 12
-        pygame.draw.polygon(window, "Steel Blue", arrow(block_center,xprime_axis,axis_width,axis_size))
-        pygame.draw.polygon(window, "Steel Blue", arrow(block_center,yprime_axis,axis_width,axis_size))
+        draw_arrow(block_center,x_axis,axis_width,axis_size,"Dark Grey","x ",label_offset=25)
+        draw_arrow(block_center,y_axis,axis_width,axis_size,"Dark Grey","y ",label_offset=25)
+        draw_arrow(block_center,xprime_axis,axis_width,axis_size,"Steel Blue","x'",label_offset=25)
+        draw_arrow(block_center,yprime_axis,axis_width,axis_size,"Steel Blue","y'",label_offset=25)
+
+        # Draw angle
+        theta_arc = -theta
+        if (theta_arc < 0):
+            draw_arc((block_center[0], block_center[1]),0.1*inner_block_size,-theta_arc,theta_arc,True,"Black",label_offset=20)
+        else:
+            draw_arc((block_center[0], block_center[1]),0.1*inner_block_size,0,-theta_arc,False,"Black",label_offset=20)
 
         # Draw stresses on outer block
         stress_offset = 10+outer_block_size
         stress_width = 8
         stress_size  = 24
-        pygame.draw.polygon(window, "Red",  arrow((block_center[0]+stress_offset,block_center[1]),(sxx,0),stress_width,stress_size))
-        pygame.draw.polygon(window, "Red",  arrow((block_center[0]+stress_offset,block_center[1]),(0,sxy),stress_width,stress_size))
-        pygame.draw.polygon(window, "Red",  arrow((block_center[0]-stress_offset,block_center[1]),(-sxx,0),stress_width,stress_size))
-        pygame.draw.polygon(window, "Red",  arrow((block_center[0]-stress_offset,block_center[1]),(0,-sxy),stress_width,stress_size))
-        pygame.draw.polygon(window, "Blue", arrow((block_center[0],block_center[1]-stress_offset),(0,syy),stress_width,stress_size))
-        pygame.draw.polygon(window, "Blue", arrow((block_center[0],block_center[1]-stress_offset),(sxy,0),stress_width,stress_size))
-        pygame.draw.polygon(window, "Blue", arrow((block_center[0],block_center[1]+stress_offset),(0,-syy),stress_width,stress_size))
-        pygame.draw.polygon(window, "Blue", arrow((block_center[0],block_center[1]+stress_offset),(-sxy,0),stress_width,stress_size))
-
-        # Write text
-        stress_offset = 50+outer_block_size
-        #window.blit(stress_font.render(str(sxx), anti_aliasing, "Red"), (block_center[0]+stress_offset+sxx,block_center[1]))
-        #window.blit(stress_font.render(str(syy), anti_aliasing, "Blue"), (block_center[0],block_center[1]+stress_offset+syy))
+        draw_arrow((block_center[0]+stress_offset,block_center[1]),(sxx,0),stress_width,stress_size, "Red", "σ-x", label_offset=45)
+        draw_arrow((block_center[0]+stress_offset,block_center[1]),(0,sxy),stress_width,stress_size, "Red", "τ-xy",label_offset=35)
+        draw_arrow((block_center[0]-stress_offset,block_center[1]),(-sxx,0),stress_width,stress_size,"Red", "σ-x", label_offset=45)
+        draw_arrow((block_center[0]-stress_offset,block_center[1]),(0,-sxy),stress_width,stress_size,"Red", "τ-xy",label_offset=35)
+        draw_arrow((block_center[0],block_center[1]-stress_offset),(0,syy),stress_width,stress_size ,"Blue","σ-y", label_offset=34)
+        draw_arrow((block_center[0],block_center[1]-stress_offset),(sxy,0),stress_width,stress_size ,"Blue","τ-yx",label_offset=47)
+        draw_arrow((block_center[0],block_center[1]+stress_offset),(0,-syy),stress_width,stress_size,"Blue","σ-y", label_offset=34)
+        draw_arrow((block_center[0],block_center[1]+stress_offset),(-sxy,0),stress_width,stress_size,"Blue","τ-yx",label_offset=47)
 
         # Draw stresses on inner block
         stress_offset = -30+inner_block_size
         stress_width = 8
         stress_size  = 24
         dx = (int(+stress_offset*cos(theta+0*pi/2)),int(+stress_offset*sin(theta+0*pi/2)))
-        pygame.draw.polygon(window, "Magenta", arrow((block_center[0]+dx[0],block_center[1]+dx[1]),(+txx*cos(theta),-txx*sin(theta)),stress_width,stress_size))
-        pygame.draw.polygon(window, "Magenta", arrow((block_center[0]+dx[0],block_center[1]+dx[1]),(+txy*sin(theta),+txy*cos(theta)),stress_width,stress_size))
-        pygame.draw.polygon(window, "Magenta", arrow((block_center[0]-dx[0],block_center[1]-dx[1]),(-txx*cos(theta),+txx*sin(theta)),stress_width,stress_size))
-        pygame.draw.polygon(window, "Magenta", arrow((block_center[0]-dx[0],block_center[1]-dx[1]),(-txy*sin(theta),-txy*cos(theta)),stress_width,stress_size))
+        draw_arrow((block_center[0]+dx[0],block_center[1]+dx[1]),(+txx*cos(theta),-txx*sin(theta)),stress_width,stress_size,"Magenta","σ-x'",label_offset=45)
+        draw_arrow((block_center[0]+dx[0],block_center[1]+dx[1]),(+txy*sin(theta),+txy*cos(theta)),stress_width,stress_size,"Magenta","τ-x'y'",label_offset=48)
+        draw_arrow((block_center[0]-dx[0],block_center[1]-dx[1]),(-txx*cos(theta),+txx*sin(theta)),stress_width,stress_size,"Magenta","σ-x'",label_offset=45)
+        draw_arrow((block_center[0]-dx[0],block_center[1]-dx[1]),(-txy*sin(theta),-txy*cos(theta)),stress_width,stress_size,"Magenta","τ-x'y'",label_offset=48)
         dx = (int(-stress_offset*cos(theta+1*pi/2)),int(-stress_offset*sin(theta+1*pi/2)))
-        pygame.draw.polygon(window, "Purple", arrow((block_center[0]+dx[0],block_center[1]+dx[1]),(+tyy*sin(theta),+tyy*cos(theta)),stress_width,stress_size))
-        pygame.draw.polygon(window, "Purple", arrow((block_center[0]+dx[0],block_center[1]+dx[1]),(+txy*cos(theta),-txy*sin(theta)),stress_width,stress_size))
-        pygame.draw.polygon(window, "Purple", arrow((block_center[0]-dx[0],block_center[1]-dx[1]),(-tyy*sin(theta),-tyy*cos(theta)),stress_width,stress_size))
-        pygame.draw.polygon(window, "Purple", arrow((block_center[0]-dx[0],block_center[1]-dx[1]),(-txy*cos(theta),+txy*sin(theta)),stress_width,stress_size))
+        draw_arrow((block_center[0]+dx[0],block_center[1]+dx[1]),(+tyy*sin(theta),+tyy*cos(theta)),stress_width,stress_size,"Purple","σ-y'",label_offset=45)
+        draw_arrow((block_center[0]+dx[0],block_center[1]+dx[1]),(+txy*cos(theta),-txy*sin(theta)),stress_width,stress_size,"Purple","τ-y'x'",label_offset=48)
+        draw_arrow((block_center[0]-dx[0],block_center[1]-dx[1]),(-tyy*sin(theta),-tyy*cos(theta)),stress_width,stress_size,"Purple","σ-y'",label_offset=45)
+        draw_arrow((block_center[0]-dx[0],block_center[1]-dx[1]),(-txy*cos(theta),+txy*sin(theta)),stress_width,stress_size,"Purple","τ-y'x'",label_offset=48)
 
         # Draw Mohr's circle
-        axis_length = window_height*0.7
+        axis_length = window_height*0.77
+        axis_height = window_height*0.6
         axis_width = 4
         axis_size  = 16
-        plot_scale = 2
-        pygame.draw.polygon(window, "Black", arrow((circle_center[0]-axis_length*0.5,circle_center[1]),(axis_length,0),axis_width,axis_size))
-        pygame.draw.polygon(window, "Black", arrow((circle_center[0],circle_center[1]+axis_length*0.5),(0,axis_length),axis_width,axis_size))
+        plot_scale = 3
+        draw_arrow((circle_center[0]-axis_length*0.5,circle_center[1]),(axis_length,0),axis_width,axis_size,"Dark Grey","σ",label_offset=30)
+        draw_arrow((circle_center[0],circle_center[1]-axis_height*0.5),(0,-axis_height),axis_width,axis_size,"Dark Grey","τ",label_offset=30)
         pygame.draw.circle(window, "Violet", (circle_center[0]+plot_scale*savg,circle_center[1]), plot_scale*tmax, 3)
 
         # Draw vectors to points
-        pygame.draw.polygon(window, "Red",     arrow((circle_center[0]+plot_scale*savg,circle_center[1]),(+plot_scale*(sxx-savg),-plot_scale*sxy), 2, 0))
-        pygame.draw.polygon(window, "Blue",    arrow((circle_center[0]+plot_scale*savg,circle_center[1]),(+plot_scale*(syy-savg),+plot_scale*sxy), 2, 0))
-        pygame.draw.polygon(window, "Magenta", arrow((circle_center[0]+plot_scale*savg,circle_center[1]),(+plot_scale*(txx-savg),-plot_scale*txy), 2, 0))
-        pygame.draw.polygon(window, "Purple",  arrow((circle_center[0]+plot_scale*savg,circle_center[1]),(+plot_scale*(tyy-savg),+plot_scale*txy), 2, 0))
+        draw_arrow((circle_center[0]+plot_scale*savg,circle_center[1]),(+plot_scale*(sxx-savg),-plot_scale*sxy), 2, 0,"Red",    "({:.0f},{:.0f})".format(sxx,sxy),label_offset=50)
+        draw_arrow((circle_center[0]+plot_scale*savg,circle_center[1]),(+plot_scale*(syy-savg),+plot_scale*sxy), 2, 0,"Blue",   "({:.0f},{:.0f})".format(syy,-sxy),label_offset=50)
+        draw_arrow((circle_center[0]+plot_scale*savg,circle_center[1]),(+plot_scale*(txx-savg),-plot_scale*txy), 2, 0,"Magenta","({:.0f},{:.0f})".format(txx,txy),label_offset=50)
+        draw_arrow((circle_center[0]+plot_scale*savg,circle_center[1]),(+plot_scale*(tyy-savg),+plot_scale*txy), 2, 0,"Purple", "({:.0f},{:.0f})".format(tyy,-txy),label_offset=50)
 
         # Draw stress points
-        pygame.draw.circle(window, "Red",        (circle_center[0]+plot_scale*sxx,circle_center[1]+plot_scale*sxy), 10)
-        pygame.draw.circle(window, "Blue",       (circle_center[0]+plot_scale*syy,circle_center[1]-plot_scale*sxy), 10)
-        pygame.draw.circle(window, "Magenta",    (circle_center[0]+plot_scale*txx,circle_center[1]+plot_scale*txy), 10)
-        pygame.draw.circle(window, "Purple",     (circle_center[0]+plot_scale*tyy,circle_center[1]-plot_scale*txy), 10)
+        pygame.draw.circle(window, "Red",     (circle_center[0]+plot_scale*sxx,circle_center[1]+plot_scale*sxy), 10)
+        pygame.draw.circle(window, "Blue",    (circle_center[0]+plot_scale*syy,circle_center[1]-plot_scale*sxy), 10)
+        pygame.draw.circle(window, "Magenta", (circle_center[0]+plot_scale*txx,circle_center[1]+plot_scale*txy), 10)
+        pygame.draw.circle(window, "Purple",  (circle_center[0]+plot_scale*tyy,circle_center[1]-plot_scale*txy), 10)
 
-        # Draw the title text
-        #title_position = (circle_center[0]-120,window_height-100) # (x,y)
-        #window.blit(title_text, title_position)
+        # Draw arc
+        arc_radius = 0.3*plot_scale*tmax
+        if (theta_arc < 0):
+            draw_arc((circle_center[0]+plot_scale*savg, circle_center[1]),arc_radius,2*thetap-2*theta_arc,2*theta_arc,True,"Black",label_offset=20)
+        else:
+            draw_arc((circle_center[0]+plot_scale*savg, circle_center[1]),arc_radius,2*thetap,-2*theta_arc,False,"Black",label_offset=20)
+
+        # Draw text
+        window.blit(stress_font.render("σ-x",  False, "Red"),  (720-620, 20-5))
+        window.blit(stress_font.render("σ-y",  False, "Blue"), (720-620, 50-5))
+        window.blit(stress_font.render("τ-xy", False, "Red"),  (640-620, 80-5))
+        window.blit(stress_font.render("=",    False, "Black"),(690-620, 80-5))
+        window.blit(stress_font.render("τ-yx", False, "Blue"), (710-620, 80-5))
+        window.blit(stress_font.render("= {:3d}".format(sxx), False, "Black"), (760-620, 20-5))
+        window.blit(stress_font.render("= {:3d}".format(syy), False, "Black"), (760-620, 50-5))
+        window.blit(stress_font.render("= {:3d}".format(sxy), False, "Black"), (760-620, 80-5))
+    
+        # Draw different widgets within the window:
+        pygame_widgets.update(events)
 
         pygame.display.update()
 
--- docs/sphinx/build/html/_sources/mohr_circle.rst.txt
+++ docs/sphinx/build/html/_sources/mohr_circle.rst.txt
@@ -1,8 +1,48 @@
 Mohr's circle
 =============
 
-An interactive stress transformation demo with Mohr's circle.
+Mohr's circle offers an intuitive way to visualize states of stress and stress transformations. The following interactive demonstration is intended to enhance one's understanding of Mohr's circle. Click on the example below to get started.
    
 .. raw:: html
 
 	 <iframe src="_static/mohr_circle/index.html" style="width:100%; height:100%; aspect-ratio: 16/8;" scrolling="no" frameborder="0"></iframe>
+
+The example above considers a block subjected to a two-dimensional state of stress, illustrated on the right side of the screen. The different components of normal and shear stress (:math:`\sigma_x`, :math:`\sigma_y`, :math:`\tau_{xy} = \tau_{yx}`) acting on the grey block whose edges are aligned with the :math:`x`-:math:`y` coordinate axes can be adjusted using the three sliders in the top left corner of the screen. The orientation of the blue block whose edges are aligned with the :math:`x'`-:math:`y'` axes can be adjusted by clicking on the blue block and rotating it. The components of normal and shear stress acting on the blue block (:math:`\sigma_{x'}`, :math:`\sigma_{y'}`, :math:`\tau_{x'y'} = \tau_{y'x'}`) are determined according to the stress transformation equations:
+	
+.. math::
+
+   \sigma_{x'} = \frac{\sigma_x + \sigma_y}{2} + \frac{\sigma_x - \sigma_y}{2} \cos (2 \theta) + \tau_{xy} \sin (2 \theta)
+   
+.. math::
+
+   \sigma_{y'} = \frac{\sigma_x + \sigma_y}{2} - \frac{\sigma_x - \sigma_y}{2} \cos (2 \theta) - \tau_{xy} \sin (2 \theta)
+   
+.. math::
+
+   \tau_{x'y'} = - \frac{\sigma_x - \sigma_y}{2} \sin (2 \theta) + \tau_{xy} \cos (2 \theta)
+
+where :math:`\theta` measures the angle between the :math:`x` and :math:`x'` coordinate axes. Try rotating the blue block to see how the state of stress in the :math:`x'`-:math:`y'` coordinate frame changes.
+
+Mohr's circle is displayed on the left side of the screen, illustrating the aforementioned states of stress in a two-dimensional diagram, with normal stress :math:`\sigma` measured on the horizontal axis and shear stress :math:`\tau` measured on the vertical axis. The red and blue dots respectively represent the states of stress corresponding to :math:`(\sigma_x,\tau_{xy})` and :math:`(\sigma_y,-\tau_{yx})`, whereas the magenta and purple dots represent the transformed states of stress corresponding to :math:`(\sigma_{x'},\tau_{x'y'})` and :math:`(\sigma_{y'},-\tau_{y'x'})`, respectively. The center of Mohr's circle is positioned along the :math:`\sigma` axis, and corresponds to the "average" normal stress:
+
+.. math::
+
+   \sigma_{\text{avg}} = \frac{\sigma_x + \sigma_y}{2}
+
+Additionally, the radius of Mohr's circle can be determined by the Pythagorean theorem:
+
+.. math::
+
+   R = \sqrt{\left( \frac{\sigma_x - \sigma_y}{2} \right)^2 + \left( \tau_{xy} \right)^2}
+
+Moreover, :math:`R` represents the magnitude of the maximum in-plane shear stress :math:`\tau_{\text{max in-plane}}` among all possible rotation angles :math:`\theta \in [-180^{\circ},+180^{\circ}]`.
+
+.. note:: If the blue block is rotated by an angle of :math:`\theta`, then the angle measured between :math:`(\sigma_x,\tau_{xy})` and :math:`(\sigma_{x'},\tau_{x'y'})` on Mohr's circle will be :math:`2 \theta` (*twice* the rotation angle).
+
+For any state of stress characterized by :math:`\sigma_x`, :math:`\sigma_y`, and :math:`\tau_{xy}`, it is always possible to find a particular rotation angle :math:`\theta_p` for which :math:`\sigma_{x'}`, :math:`\sigma_{y'}` correspond to the principal stresses :math:`\sigma_{1,2} = \sigma_{\text{avg}} \pm R`. In particular:
+
+.. math::
+
+   \tan (2 \theta_p) = \frac{\tau_{xy}}{(\sigma_x - \sigma_y)/2}
+
+Try to prove this to yourself by changing the values of :math:`\sigma_x`, :math:`\sigma_y`, :math:`\tau_{xy}` and attempting to find the value of :math:`\theta_p` for which :math:`\tau_{x'y'} = 0`, and notice how the plotted points for :math:`\sigma_{x'}`, :math:`\sigma_{y'}` in Mohr's circle appear on the normal stress axis.
